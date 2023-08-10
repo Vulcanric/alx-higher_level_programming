247625517370a68242baea5ffb95a80d45752930
Vulcanric <jneric49@gmail.com>
2023-08-10 03:57:36 +0100

    prints a float to a two precision value

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -2,9 +2,8 @@
 number = 3.14159
 
 if not isinstance(number, float):
-    number = float(number)
-
-if number >= 0:
+    print(f"Float: {number}")
+elif number >= 0:
     print(f"Float:{number: .2f}")
 else:
     print(f"Float: {number:.2f}")
