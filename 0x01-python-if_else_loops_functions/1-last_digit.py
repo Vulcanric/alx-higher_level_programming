2b04ceceedc4874d650e06480ffd81223b248f8c
Vulcanric <jneric49@gmail.com>
2023-08-14 03:07:40 +0100

    control statements

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = number % -10
+
+ld = last_digit
+
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is zero")
+elif last_digit != 0 and last_digit < 6:
+    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
