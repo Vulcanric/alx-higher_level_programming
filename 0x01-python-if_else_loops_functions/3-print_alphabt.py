626efc9c41ff8bf5646e1ec7356331e06e01c01e
Vulcanric <jneric49@gmail.com>
2023-08-14 03:57:58 +0100

    prints the ascii alphabets, in lowercase, except from 'q' and 'e'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# prints the ascii alphabets in lowercase
+# except from letter 'e' and 'q'
 for i in range(97, 123):
     if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end = '')
+        print("{}".format(chr(i)), end='')
