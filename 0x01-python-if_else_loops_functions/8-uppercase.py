c40b1fd7ab5d3bb63200baf348a49546c5da80af
Vulcanric <jneric49@gmail.com>
2023-08-15 03:06:19 +0100

    Function that prints a string in uppercase followed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,13 +7,11 @@ def uppercase(str):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             # Convert to uppercase ascii number
             upper = ord(str[i]) - 32  # 'a' - 32 = 'A' and so on ...
-
         # otherwise let the char remain the same
         else:
             upper = ord(str[i])
-
-        # Print the ascii upper letter from ascii number
+        # Prints the ascii upper letter from ascii number
         print("{}".format(chr(upper)), end='')
         i += 1
 
-    print("{}".format('\n'), end='')  # print newline after all done
+    print("{}".format('\n'), end='')  # Prints newline after all done
