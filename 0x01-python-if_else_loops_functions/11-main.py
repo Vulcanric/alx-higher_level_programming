c25994544a7e83e6d027137c10e60b0aca9d1b69
Vulcanric <jneric49@gmail.com>
2023-08-15 04:01:40 +0100

    Implement power calculation function

---
+++ 0x01-python-if_else_loops_functions/11-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+pow = __import__("11-pow").pow
+
+# Testing the function pow() from the file '11-pow.py'
+print(pow(2, 2))
+print(pow(98, 0))
+print(pow(98, 2))
+print(pow(100, -2))
+print(pow(10, -2))
+print(pow(-4, 5))
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Computes a to the power of b and returns the value
+    """
+    return (a ** b)
