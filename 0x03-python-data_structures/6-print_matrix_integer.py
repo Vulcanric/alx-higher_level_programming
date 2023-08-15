b3797d96bfd5bc45562c08c93cb893c27f0870ec
Vulcanric <jneric49@gmail.com>
2023-08-15 05:59:46 +0100

    prints a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers
+    """
+    for row in matrix:
+        for num in row:
+            print("{}".format(num))
