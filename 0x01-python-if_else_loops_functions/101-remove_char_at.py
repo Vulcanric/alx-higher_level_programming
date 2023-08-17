ae926ed7e28b6daa57233d4145b32d0462c42085
Vulcanric <jneric49@gmail.com>
2023-08-17 15:39:14 +0100

    Creates a copy of a string removing the character at specified index

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    """Creates a copy of the string str, removing the character at position n
+    C array style
+    """
+    str_cpy = ""
+
+    i = 0;
+    while i < len(str):
+        if i != n:
+            str_cpy += str[i]
+        i += 1
+
+    return str_cpy
