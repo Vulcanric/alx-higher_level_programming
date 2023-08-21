88f9520f88b39b444bc5124c9927cc2003c3e9a9
Vulcanric <jneric49@gmail.com>
2023-08-21 13:44:31 +0100

    program that prints the sum of an indefinite number of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,13 @@
 
 from sys import argv
 
-num_of_args = len(argv)
-result = 0
-index = 1
-while index < num_of_args:
-    num = int(argv[index])
-    result += num
-    index += 1
+if __name__ == "__main__":
+    num_of_args = len(argv)
+    result = 0
+    index = 1
+    while index < num_of_args:
+        num = int(argv[index])
+        result += num
+        index += 1
 
-print(result)
+    print(result)
