b2ff2be3d679caaba0c4d59f3c3fc2ad666a6272
Vulcanric <jneric49@gmail.com>
2023-08-30 05:46:03 +0100

    update: defined a public instance method called area()

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        """Initializes a Square instance
+
+        args:
+            self: the instance or object created through class
+            size (int): size of the square
+
+        Return:
+            returns nothing
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Computes the area of square
+
+        args:
+            self: the instance or object created through class
+
+        Return:
+            returns the area
+        """
+        return (self.__size ** 2)
