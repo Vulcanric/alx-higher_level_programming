aef10549282e6527455f635623359d674c7f2c78
Vulcanric <jneric49@gmail.com>
2023-08-31 03:03:01 +0100

    update: added doc strings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""Defined a Class"""
+
+
 class Square:
+    """Defined a Class ``Square``
+    and this time, initialized instances"""
     def __init__(self, size):
+        """Initialized instances/object of the class"""
         self.__size = size
