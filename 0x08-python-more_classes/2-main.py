95a4350ddf609be8f7c447455d998a1699ef9ded
Vulcanric <jneric49@gmail.com>
2023-09-05 06:39:37 +0100

    update: added public instance methods: area() and perimeter()

---
+++ 0x08-python-more_classes/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+Rectangle = __import__('2-rectangle').Rectangle
+
+my_rectangle = Rectangle(3, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+my_rectangle.width = 4
+my_rectangle.height = 6
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""This module contains more updated definition of a class ``Rectangle``
+than other previous modules"""
+
+
+class Rectangle:
+    """Creates an object or instance of type Rectangle
+    class ``Rectangle`` has the following:
+    Private instance attribute called ``width``:
+        getter method to retrieve it
+        setter method to set it
+    Private instance attribute ``height``:
+        getter method to retrieve it
+        setter method to set it
+    Public instance method area():
+        Returns the area of Rectangle
+    Public instance method perimeter():
+        Returns the perimeter of Rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """Instantiate Object/instances created from this class
+        :param width (int): private attribute, width of a Rectangle
+        :param height (int): private attribute,height of a Rectangle
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """Property (height) getter
+        :returns: the value of private attribute ``height``
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property (height) setter
+        Sets/resets the value of private attribute ``height``
+        :returns: nothing
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Property (width) getter
+        :returns: the value of private attribute ``width``
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property (width) setter
+        Sets/resets the value of private attribute ``width``
+        :returns: nothing
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """Returns the area Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the sum of the total sides of the rectangle"""
+        l = self.__height
+        b = self.__width
+        perim = 2 * (l + b)
+        return perim
