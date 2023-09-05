c2537e9b863c088e7f395d9f32fa86937d6fe1a4
Vulcanric <jneric49@gmail.com>
2023-09-05 05:19:04 +0100

    simple class: does nothing

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains definition of a class ``Rectangle``
+The class ``Rectangle`` does nothing
+"""
+
+
+class Rectangle:
+    """Class Rectangle:
+    The class is simple and does nothing
+    """
+    pass
