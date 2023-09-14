b6a7ca2c78cceaa35fa52405ba68265f6ef19d80
Vulcanric <jneric49@gmail.com>
2023-09-14 04:47:31 +0100

    write Python object to a text file using JSON string representation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Defines a function that writes a Python object to a text file
-    using JSON string representation"""
+using JSON string representation"""
+import json
 
 
-    import json
-
-    def save_to_json_file(my_obj, filename):
-        """Writes a Python object to a text file using JSON string
-        representation
-        :param my-obj: object to write
-        :param filename: text file to write to
-        :returns: nothing (None)
-        """
-        with open(filename, 'w') as file:
-            json.dump(my_obj, file)
+def save_to_json_file(my_obj, filename):
+    """Writes a Python object to a text file using JSON string
+    representation
+    :param my-obj: object to write
+    :param filename: text file to write to
+    :returns: nothing (None)
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
