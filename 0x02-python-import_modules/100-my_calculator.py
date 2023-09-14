0cc51ab12f0255c324064ace247ed1f126411463
Vulcanric <jneric49@gmail.com>
2023-09-14 23:29:29 +0100

    performs basic calculation operation (a calculator) fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -23,18 +23,17 @@ command line arguments
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-
-if argv[2] not in ['+', '-', '*', '/']:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-
-a, b = int(argv[1]), int(argv[3])
-
 if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a, b = int(argv[1]), int(argv[3])
+
     if argv[2] == '+':
         print(f"{a} + {b} =", add(a, b))
     elif argv[2] == '-':
