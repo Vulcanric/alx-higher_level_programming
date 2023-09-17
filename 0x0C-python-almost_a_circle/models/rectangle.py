86ccac45f8648ba499381de1101a5553880df823
Vulcanric <jneric49@gmail.com>
2023-09-17 05:39:40 +0100

    update: added parameter type validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,8 +17,28 @@ class Rectangle(Base):
     Every object created from this class inherits its id from the
     'Base' class
     """
+    def validate_parameters(w=1, h=1, x=0, y=0):
+        if type(w) is not int:
+            raise TypeError("width must be an integer")
+        if w <= 0:
+            raise ValueError("width must be > 0")
+        if type(h) is not int:
+            raise TypeError("height must be an integer")
+        if h <= 0:
+            raise ValueError("height must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
     # The Constructor
     def __init__(self, width, height, x=0, y=0, id=None):
+        # check for invalid parameter types
+        Rectangle.validate_parameters(width, height, x, y)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -48,16 +68,20 @@ class Rectangle(Base):
     # Setters
     @width.setter
     def width(self, width):
+        Rectangle.validate_parameters(w=width)
         self.__width = width
 
     @height.setter
     def height(self, height):
+        Rectangle.validate_parameters(h=height)
         self.__height = height
 
     @x.setter
-    def x(self, x):
-        self.__x = x
+    def x(self, x_value):
+        Rectangle.validate_parameters(x=x_value)
+        self.__x = x_value
 
     @y.setter
-    def y(self, y):
-        self.__y = y
+    def y(self, y_value):
+        Rectangle.validate_parameters(y=y_value)
+        self.__y = y_value
