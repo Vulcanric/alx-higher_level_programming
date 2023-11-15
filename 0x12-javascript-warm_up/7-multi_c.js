ea3ab7c4674c8bb5423f9614337bd5c28532cb60
Vulcanric <jneric49@gmail.com>
2023-11-15 16:25:15 +0100

    More loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@
 const numberOfOccurrence = parseInt(process.argv[2]);
 
 if (!numberOfOccurrence) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < numberOfOccurrence; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
