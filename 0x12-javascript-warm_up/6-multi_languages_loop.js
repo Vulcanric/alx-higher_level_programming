7d99f8e0440f83b16c3761b236c60ddaa90f82f6
Vulcanric <jneric49@gmail.com>
2023-11-15 15:54:59 +0100

    [fix]: (Loops) made code semistandard validated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 /* Using loops */
 
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i < 3; i++)
-{
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
   console.log(languages[i]);
 }
