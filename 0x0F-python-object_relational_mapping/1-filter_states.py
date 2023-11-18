c63524c6e642d88e8bf4a39202f4f40411697524
Vulcanric <jneric49@gmail.com>
2023-11-18 17:01:17 +0100

    Querys database table for rows with `name` starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     results = cursor.fetchall()
     for result in results:
