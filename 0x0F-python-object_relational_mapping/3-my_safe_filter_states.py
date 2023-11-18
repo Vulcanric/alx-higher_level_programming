43a53ab976819015a5a5d6d38a84db2a26bbe53c
Vulcanric <jneric49@gmail.com>
2023-11-18 21:03:39 +0100

    Protects from SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Contains a script that queries a database for a list of rows
+with `name` as the one passed to the cmdline from a table, `states`
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    statename = argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=username, passwd=password,
+            db=database, charset='utf8'
+            )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = %s", statename)
+
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+
+    cursor.close()
+    db.close()
