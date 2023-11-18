2b025955859e85f8d4f2f63f2b33422d37367dde
Vulcanric <jneric49@gmail.com>
2023-11-18 16:57:51 +0100

    Querys database table for rows with `name` starting with 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Contains a script that queries a database for a list of rows
+with `name` starting with 'N' from a table `states`
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=username, passwd=password,
+            db=database
+            )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
