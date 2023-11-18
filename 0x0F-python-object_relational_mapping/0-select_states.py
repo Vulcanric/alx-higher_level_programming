1fb408584e9bd8f349f59ef6ef25e48618992bbd
Vulcanric <jneric49@gmail.com>
2023-11-18 14:38:55 +0100

    Creates table `stat` and query table for it's rows

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 
+Contains a script that lists all states from the database `hbtn_0e_0_usa`
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=username, passwd=password,
+            db=database
+        )
+
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = curr.fetchall()
+
+    for state in states:
+        print(state)
