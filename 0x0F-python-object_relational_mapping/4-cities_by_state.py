88934324443cddb7b6f5a439b6acde336c87c5a0
Vulcanric <jneric49@gmail.com>
2023-11-18 22:34:13 +0100

    Introduce foreign key relationship

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-    "SELECT cities.id, cities.name, states.name\
-            FROM cities, states WHERE cities.state_id = states.id\
-            ORDER BY cities.id ASC"
+        "SELECT cities.id, cities.name, states.name\
+        FROM cities, states WHERE cities.state_id = states.id\
+        ORDER BY cities.id ASC"
             )
 
     cities = cursor.fetchall()
