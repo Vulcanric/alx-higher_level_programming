85742b2a2800f31058c1febed1b4f176eb1fade8
Vulcanric <jneric49@gmail.com>
2023-11-20 01:37:53 +0100

    Queries database for all the states along with their id

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,26 +8,27 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from urllib.parse import quote
+from urllib.parse import quote  # Used to encode special character
 
+if __name__ == "__main__":
+    # Get MySQL database info
 
-# Get MySQL database info
-username = sys.argv[1]
-password = quote(sys.argv[2])
-databasename = sys.argv[3]
+    username = sys.argv[1]
+    password = quote(sys.argv[2])
+    databasename = sys.argv[3]
 
-# Create connection engine
-engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{databasename}',
-        pool_pre_ping=True, echo=True
-        )
+    # Create connection engine
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost/{databasename}',
+            pool_pre_ping=True, echo=True
+            )
 
-# Create a session inorder to query the database
-Session = sessionmaker()
-session = Session(bind=engine)
+    # Create a session inorder to query the database
+    Session = sessionmaker()
+    session = Session(bind=engine)
 
-states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
 
-# Prints all states id along with their names
-for state in states:
-    print(f'{state.id}: {state.name}')
+    # Prints all states id along with their names
+    for state in states:
+        print(f'{state.id}: {state.name}')
