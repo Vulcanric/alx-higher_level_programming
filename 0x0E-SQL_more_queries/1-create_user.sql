c878922430593a600e9a4193a71b4d5e0d6919f5
Vulcanric <jneric49@gmail.com>
2024-02-17 18:35:42 +0100

    Create a new user having all privileges as the root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create a new user having all privilege attached to it
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'User_0d_1_pwd_@';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
