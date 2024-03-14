986acd34c18e3c0a7bc1bd3d69f0bfbe85441c09
Vulcanric <jneric49@gmail.com>
2024-03-14 05:53:06 +0100

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+languagesILearnt = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+languagesILearnt.forEach((languageDescription) => {
+  console.log(languageDescription);
+});
