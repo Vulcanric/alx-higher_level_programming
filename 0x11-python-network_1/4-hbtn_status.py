142fd68b2dd4484fff6615b2437bb21d4dd4b7ba
Vulcanric <jneric49@gmail.com>
2024-06-21 16:06:20 +0100

    Fetches a resource from a URL using the `requests` package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches a resource from hard-coded URL using the `requests` package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(response.text))
+    print('\t- content:', response.text)
