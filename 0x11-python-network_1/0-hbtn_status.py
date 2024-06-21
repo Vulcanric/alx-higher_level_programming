ae8191939baf6809c3b27cd98e27af7fb702cc00
Vulcanric <jneric49@gmail.com>
2024-06-21 13:37:19 +0100

    A script that fetches a URL using Python's `urllib` package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,17 +2,17 @@
 """ This module uses the `urllib` Python library to retrieve a resource
 from a URL
 """
-import urllib
+import urllib.request
 
 if __name__ == "__main__":
 
-    req = __import__('urllib.request').request
+    # req = __import__('urllib.request').request
 
     url = 'https://alx-intranet.hbtn.io/status'
 
-    with req.urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print('Body response:')
         print('\t- type:', type(body))
-        print('\t- content:', body)         # body is type 'byte' by default
-        print('\t- utf8 content:', body.decode())  # Default encoding is utf-8
+        print('\t- content:', body)  # body in byte b''
+        print('\t- utf8 content:', body.decode())  # UTF-8 representation ''
