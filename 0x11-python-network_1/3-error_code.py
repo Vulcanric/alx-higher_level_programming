9b460cffa70b7741033491ef8cd426502b995c4b
Vulcanric <jneric49@gmail.com>
2024-06-21 15:57:21 +0100

    Retrieves a resource from a url or error code if an error occured

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,16 +2,18 @@
 """ This module uses the `urllib` library to retrieve a responce header
     from a URL
 """
-import sys
 import urllib
+import sys
 
 
 if __name__ == "__main__":
 
+    url = sys.argv[1]  # Receives the URL as a commandline argument
+
     request = __import__('urllib.request').request
 
-    url_ = sys.argv[1]  # Receives the URL as commandline argument
-    with request.urlopen(url_) as resp:
-
-        print(resp.msg)
-        print("Error code: {resp.code}")
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode())  # Response body decoded in UTF-8
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
