762991387e507520fca82b7599f063c63942df40
Vulcanric <jneric49@gmail.com>
2024-06-22 06:44:07 +0100

    Fetches a resource from a URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,10 @@ import urllib
 if __name__ == "__main__":
 
     url = 'https://alx-intranet.hbtn.io/status'
+    req = __import__('urllib.request').request
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+    with req.urlopen(url) as response:
+        body = response.read()  # Response is a file object
         print('Body response:')
         print('\t- type:', type(body))
         print('\t- content:', body)  # body in byte b''
